ab34d20373373d7d9e169bd8a2a713dc4a0c9366
Nayana62 <nmnayana62@gmail.com>
2023-07-22 15:55:16 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import Editor from "./Editor";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+function App() {
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [javascript, setJavascript] = useLocalStorage("javascript", "");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${javascript}</script>
+    </html>
+    `);
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, javascript]);
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={javascript}
+          onChange={setJavascript}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
